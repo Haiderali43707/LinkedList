1f333b8ef0e3c848c717c71692ed5db30b26c651
Haiderali43707 <105425518+Haiderali43707@users.noreply.github.com>
2022-05-17 23:30:02 +0530

    Create Remove duplicate element from sorted Linked List .java

---
+++ Remove duplicate element from sorted Linked List .java
@@ -0,0 +1,21 @@
+class GfG
+{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head)
+    {
+	// Your code here	
+	Node curr=head;
+	while(curr.next!=null)
+	{
+	    if(curr.data==curr.next.data)
+	    {
+	        curr.next=curr.next.next;
+	    }
+	    else
+	    {
+	        curr=curr.next;
+	    }
+	}
+	return head;
+    }
+}
