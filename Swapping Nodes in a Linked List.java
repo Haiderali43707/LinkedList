52140cfee2f1da3cc4cb174226342f23f4bd78b7
Haider Ali <105425518+Haiderali43707@users.noreply.github.com>
2022-08-26 21:29:11 +0530

    Update Swapping Nodes in a Linked List.java

--- Swapping Nodes in a Linked List.java
+++ Swapping Nodes in a Linked List.java
@@ -5,7 +5,7 @@ class Solution {
         ListNode sec=head;
         ListNode slow=head;
         ListNode fast=head;
-       for(int i=0;i<k-1;++i)
+       for(int i=0;i<k-1;i++)
        {
            fast=fast.next;
            first=fast;
