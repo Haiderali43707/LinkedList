1a31a4647e61dab106127a576fa53dd26307a1bd
Haider Ali <105425518+Haiderali43707@users.noreply.github.com>
2022-08-26 21:27:32 +0530

    Create Swapping Nodes in a Linked List.java

---
+++ Swapping Nodes in a Linked List.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+       
+        ListNode first=head;
+        ListNode sec=head;
+        ListNode slow=head;
+        ListNode fast=head;
+       for(int i=0;i<k-1;++i)
+       {
+           fast=fast.next;
+           first=fast;
+       }
+        while(fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+        sec=slow;
+        int temp=first.val;
+        first.val=sec.val;
+        sec.val=temp;
+        return head;
+       
+        
+        
+    }
+}
